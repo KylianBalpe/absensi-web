a8f0063f70130940a6d9ebfbbca0538b6d93dcd0
Iqbal Pamula <balpamula@gmail.com>
2024-07-30 21:02:53 +0700

    feat: login register page

---
+++ src/app/(with-nav)/layout.tsx
@@ -0,0 +1,22 @@
+import Navbar from "@/components/ui/navbar";
+import Sidebar from "@/components/ui/sidebar";
+
+import React from "react";
+
+const Layout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <div className="flex min-h-screen w-full flex-col">
+      <Sidebar />
+      <div className="sm:pl-48">
+        <Navbar />
+        <div className="p-4 sm:p-6">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/components/ui/links.ts
@@ -0,0 +1,21 @@
+import { FishIcon, HomeIcon, TreesIcon } from "lucide-react";
+
+const LinksItem = [
+  {
+    label: "Home",
+    icon: HomeIcon,
+    path: "/dashboard",
+  },
+  {
+    label: "Fish",
+    icon: FishIcon,
+    path: "/dashboard/fish",
+  },
+  {
+    label: "Plants",
+    icon: TreesIcon,
+    path: "/dashboard/plants",
+  },
+];
+
+export default LinksItem;
