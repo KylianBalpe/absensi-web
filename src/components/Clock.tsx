0b5f95e0e72ba7145a2282b9326876447b41e139
iniadittt <adityabayu861@gmail.com>
2024-07-31 16:01:27 +0700

    update

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,25 +1,44 @@
 "use client";
-
 import { useEffect, useState } from "react";
 
+const monthName = [
+    "Januari",
+    "Februari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember",
+  ];
+
 const Clock = () => {
-  const [time, setTime] = useState("");
+  const [dateTime, setDateTime] = useState("");
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setTime(
-        new Date().toLocaleTimeString([], {
-          hour: "2-digit",
-          minute: "2-digit",
-          second: "2-digit",
-          hour12: false,
-        }),
-      );
+      const now = new Date();
+      const timeString = now.toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false,
+      });
+      const dayName = now.toLocaleDateString("id-ID", { weekday: "long" });
+      const year = now.getFullYear();
+      const month = now.getMonth();
+      const day = String(now.getDate()).padStart(2, "0");
+      const dateString = `${day} ${monthName[month]} ${year}`;
+      setDateTime(`${dayName}, ${dateString} - ${timeString} WIB`);
     }, 1000);
     return () => clearInterval(interval);
   }, []);
 
-  return <p>{time ? `${time} WIB` : "Loading..."}</p>;
+  return <div>{dateTime || "Loading..."}</div>;
 };
 
 export default Clock;
